7b464f165c74c5c7b02278488ab087eadfa8afc5
Alexander White <alex121994@gmail.com>
2021-05-08 23:47:25 +0300

    0.0.2
    - Jumping and squating added

--- js/main.js
+++ js/main.js
@@ -6,8 +6,13 @@
  document.addEventListener('keydown', event => {
      if (isGameStarted) {
         if (event.code == "ArrowUp" || event.code == "Space") jump()
+        if (event.code == "ArrowDown") squat()
      } else if (event.code == "ArrowUp" || event.code == "Space") startNewGame()
 
+ })
+
+ document.addEventListener('keyup', event => {
+     if (isGameStarted && event.code == "ArrowDown") unsquat()
  })
  
  /**
@@ -30,10 +35,15 @@ let speedInterval = 100
  */
  let isGameStarted = false
 
- /**
-  * True if the dino is in jump.
-  */
+/**
+ * True if the dino is in jump.
+ */
  let isInJump = false
+
+/**
+ * True if the dino is squating.
+ */
+let isSquating = false
 
 /**
  * Score in the current game.
@@ -43,7 +53,7 @@ let score = 0
 /**
  * Intervals.
  */
-let intervalDinoAnimation, intervalScores
+let intervalDinoAnimation, intervalScores, jumpInterval, squatInterval
 
 /**
  * Starts a new game.
@@ -53,11 +63,12 @@ function startNewGame() {
     nodeDino.classList.add('dino-1')
     intervalDinoAnimation = setInterval(animateDino, 100)
     intervalScores = setInterval(runScore, 100)
-
 }
 
 function animateDino() {
+    if (isSquating) return nodeDino.classList.toggle('dino-down-2')
     nodeDino.classList.toggle('dino-2')
+
 }
 
 function runScore() {
@@ -72,24 +83,58 @@ function jump() {
     if (isInJump) return
     isInJump = true
 
-    let dinoPosition = -16
+    let dinoPosition = -40
     let pause = 20
     let movingUp = true
 
-    let jumpInterval = setInterval(() => {
-        if (movingUp && dinoPosition < 128) return nodeDino.style.bottom = `${dinoPosition = dinoPosition + 8}px`
-        if (movingUp && dinoPosition < 148) return nodeDino.style.bottom = `${dinoPosition = dinoPosition + 4}px`
-        if (movingUp && dinoPosition < 160) return nodeDino.style.bottom = `${dinoPosition = dinoPosition + 2}px`
+    jumpInterval = setInterval(() => {
+        if (movingUp && dinoPosition < 128) return nodeDino.style.bottom = `${dinoPosition += 8}px`
+        if (movingUp && dinoPosition < 148) return nodeDino.style.bottom = `${dinoPosition += 4}px`
+        if (movingUp && dinoPosition < 160) return nodeDino.style.bottom = `${dinoPosition += 2}px`
         movingUp = false
         // if (pause) return pause--
-        if (dinoPosition > 148) return nodeDino.style.bottom = `${dinoPosition = dinoPosition - 2}px`
-        if (dinoPosition > 128) return nodeDino.style.bottom = `${dinoPosition = dinoPosition - 4}px`
-        if (dinoPosition > -16) return nodeDino.style.bottom = `${dinoPosition = dinoPosition - 8}px`
-        if (dinoPosition == -16) {
+        if (dinoPosition > 148) return nodeDino.style.bottom = `${dinoPosition -= 2}px`
+        if (dinoPosition > 128) return nodeDino.style.bottom = `${dinoPosition -= 4}px`
+        if (dinoPosition > -40) return nodeDino.style.bottom = `${dinoPosition -= 8}px`
+        if (dinoPosition == -40) {
             isInJump = false
             clearInterval(jumpInterval)
         } 
     }, 10)
+}
+
+function squat() {
+
+    if (isInJump) {
+        isInJump = false
+        clearInterval(jumpInterval)
+
+        let dinoPosition = nodeDino.style.bottom.slice(0, -2)
+    
+        squatInterval = setInterval(() => {
+            if (dinoPosition <= -12) {
+                nodeDino.style.bottom = `-72px`
+                isSquating = true
+                nodeDino.classList.add('dino-down-1')
+                return clearInterval(squatInterval)
+            }
+            nodeDino.style.bottom = `${dinoPosition -= 12}px`
+        }, 10)
+
+    } else {
+        isSquating = true
+        nodeDino.style.bottom = `-72px`
+        nodeDino.classList.add('dino-down-1')
+    }
+
+}
+
+function unsquat() {
+    isSquating = false
+    nodeDino.classList.remove('dino-down-1')
+    nodeDino.classList.remove('dino-down-2')
+    nodeDino.style.bottom = `-40px`
+    
 }
 
 function endGame() {
